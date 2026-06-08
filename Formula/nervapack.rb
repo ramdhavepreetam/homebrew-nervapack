b6f8d95a12fc4099e375496ba949d7d9d8494a34
Preetam Ramdhave <ramdhavepreetam@gmail.com>
2026-06-07 22:23:19 -0700

    feat: Bump nervapack formula to v0.3.0

--- Formula/nervapack.rb
+++ Formula/nervapack.rb
@@ -3,8 +3,8 @@ class Nervapack < Formula
   homepage "https://github.com/ramdhavepreetam/NervaPack"
   # Update url and sha256 after running: pip download nervapack==<ver> --no-deps --no-binary :all:
   # then: shasum -a 256 nervapack-<ver>.tar.gz
-  url "https://files.pythonhosted.org/packages/source/n/nervapack/nervapack-0.2.0.tar.gz"
-  sha256 "89c0a99b7f6d0cc849e4ea11149dbd5366fcda46e85ae111dec5cc41a2309bd7"
+  url "https://files.pythonhosted.org/packages/source/n/nervapack/nervapack-0.3.0.tar.gz"
+  sha256 "76c880ba3897923de8e37afed5f34efeff940ef915ce5ea0cf4404524fdf3c03"
   license "MIT"
 
   depends_on "python@3.12"
